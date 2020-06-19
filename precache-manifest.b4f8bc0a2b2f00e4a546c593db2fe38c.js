7ee1c6acc84e609f326c3ae33742828942674638
Mithun <mas@extremenetworks.com>
2020-06-19 21:08:57 +0530

    Updates

---
+++ precache-manifest.b4f8bc0a2b2f00e4a546c593db2fe38c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4a498cae7af7cf5302517090823c5a",
+    "url": "/speedtyping/index.html"
+  },
+  {
+    "revision": "ec6f48b132db415f1b1c",
+    "url": "/speedtyping/static/css/main.10af6cbe.chunk.css"
+  },
+  {
+    "revision": "ef5ca6c259a8d3055be7",
+    "url": "/speedtyping/static/js/2.a6637be3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speedtyping/static/js/2.a6637be3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6f48b132db415f1b1c",
+    "url": "/speedtyping/static/js/main.2f5ac0b3.chunk.js"
+  },
+  {
+    "revision": "d3de776b8179145ae2b4",
+    "url": "/speedtyping/static/js/runtime-main.2e8c3908.js"
+  }
+]);
